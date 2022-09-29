407a390785f2ab204b38e41ad58594a20f78c0cb
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2022-09-29 16:30:16 -0700

    update

--- portfolio.js
+++ portfolio.js
@@ -36,3 +36,11 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
